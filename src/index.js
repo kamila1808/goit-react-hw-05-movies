df3570bec05b05d1e0517012d355ab6700a849dd
Kamila Nauryzbayeva <kamila.nryzbv@gmail.com>
2022-12-19 19:02:56 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/kamila1808/goit-react-hw-05-movies/">
+    <BrowserRouter basename="/https://github.com/kamila1808/goit-react-hw-05-movies/">
     <App />
     </BrowserRouter>
   </React.StrictMode>
